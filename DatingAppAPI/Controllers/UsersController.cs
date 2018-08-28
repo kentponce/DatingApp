e8484773ab94c2adc4c6e3ec3d1a023a01086fec
kentponce.dev <kentponce.dev@gmail.com>
2018-08-28 14:35:16 +0800

    End of Section 12

--- DatingAppAPI/Controllers/UsersController.cs
+++ DatingAppAPI/Controllers/UsersController.cs
@@ -34,7 +34,7 @@ namespace DatingAppAPI.Controllers
             return Ok(userToReturn);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetUser")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repo.GetUser(id);
--- DatingAppAPI/Helpers/AutoMapperProfiles.cs
+++ DatingAppAPI/Helpers/AutoMapperProfiles.cs
@@ -27,6 +27,7 @@ namespace DatingAppAPI.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
